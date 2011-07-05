d5d0c95acd735cb1c6caf18a472101fec9f8dba3
Josh Larson <joshlarson@github.com>
2011-07-05 07:55:47 +0000

    Seems logical --- main should be at the root of the source tree

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+//#include "josh/ast/ast.h" 
+//#include "josh/ir/ir.h"
+//#include "josh/system/system.h"
+//#include "josh/token/token.h"
+#include <iostream>
+#include "config.h"
+
+using namespace std;
+
+int main() {
+  /*  get_tokens();
+  AST ast;
+  ast.createIR();
+
+  optimize_highlevel();
+  highlevel_to_lowlevel();
+  optimize_lowlevel();
+
+  lowlevel_to_assembly();
+  assemble();
+  //link(); // you don't both link and save to file
+  save_to_file();*/
+
+  cout << "Hello World!\n";
+  cout << "Welcome to " << PACKAGE_STRING  << "\n";
+
+  return 0;
+}

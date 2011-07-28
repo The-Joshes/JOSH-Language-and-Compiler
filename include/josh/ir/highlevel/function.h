82a7ce52cb1a2d5654c853537b5f32e94d2d5c49
Josh <jebusch@gmail.com>
2011-07-27 21:57:00 -0400

    Additional updates to the High Level IR API, including the addition of the Function level Abstraction. Remaining work: Types, Constants, CallInsts, Modules, and Profiles.

---
+++ include/josh/ir/highlevel/function.h
@@ -0,0 +1,47 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#include <cstring> // for NULL
+#include <list>
+
+class BasicBlock;
+class Module;
+class FunctionType;
+
+/// class Function
+/// Represents a list of basic blocks that compose a function.
+/// A function may have only one entry point, but multiple exit points.
+/// A function's return Type and argument Types are initialized at creation
+/// and cannot be changed afterwards.
+/// A function is not considered valid for compilation until an entryPoint is specified.
+class Function
+{
+public:
+  static Function* Create(Module *parent,
+                          FunctionType *type,
+                          BasicBlock *entryPoint=NULL);
+  
+  BasicBlock* getEntryPoint();          ///< @see entryPoint
+  void setEntryPoint(BasicBlock*);      ///< @see entryPoint
+  const FunctionType* getType() const;  ///< contains the return type and argument type(s)
+
+  //  getBlocks()
+  /// A function is composed of a list of BasicBlocks.
+  /// The order of BB in a function is arbitrary; by convention, the entry point
+  /// should be listed first, but this is not necessary.  The entry point is 
+  /// determined by the Create and/or setEntryPoint functions.
+  /// Just because a BB is in a function doesn't mean it is reachable from
+  /// normal flow of control.  
+  std::list<BasicBlock*> getBlocks(); 
+
+private:
+  Function(Module*, FunctionType*, BasicBlock*);
+
+  Module *parent;
+  FunctionType *type;
+
+  /// The point of entrance for when a function is called.
+  BasicBlock *entryPoint;
+};
+
+#endif

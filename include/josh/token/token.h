856b6f582257c3b5f657dc37f817a96a5310f497
Josh Larson <theMutatedShrimp@gmail.com>
2011-07-31 01:28:54 -0400

    Added the beginnings of an API for the tokenizer (presumably to be used by the AST constructor)

--- include/josh/token/token.h
+++ include/josh/token/token.h
@@ -1,8 +1,44 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+#include <iostream>
+
 namespace josh {
-  void get_tokens();
+********************************************************************************
+  enum TokenType {
+    /* Identifier */
+    
+    // This token type corresponds to any token that is a valid variable name,
+    // beginning with a letter or underscore (a-z, A-Z, _), and continuing with
+    // a sequence of letters, numbers, and underscores until the next character
+    // is not one of those (operator or space, most likely)
+    TT_IDENTIFIER,
+    
+    
+    /* Literals */
+    
+    // One of two options
+    // ', then a single character other than \, followed by a closing '
+    // '\, then a single valid escape character (\nt0), followed by a closing '
+    TT_CHAR_LITERAL,
+    TT_INT_LITERAL,
+    TT_BOOL_LITERAL,
+    TT_FLOAT_LITERAL,
+    TT_STRING_LITERAL,
+  };
+  
+  struct Token {
+    TokenType type;
+    union {
+      char char_val;
+      int int_val;
+      bool bool_val;
+      float float_val;
+      char *str_val;
+    };
+  };
+
+  void get_tokens(std::istream&);
 }
 
 #endif /* TOKEN_H */

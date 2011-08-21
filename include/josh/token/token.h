9513b8521e4ba6b40329d212c93273a33827a621
Josh Larson <theMutatedShrimp@gmail.com>
2011-08-21 19:11:04 -0400

    Changed comments to doxygen style. Have decided not to use the current version of tokens...

--- include/josh/token/token.h
+++ include/josh/token/token.h
@@ -3,30 +3,39 @@
 
 #include <iostream>
 
-namespace josh {
+namespace jtoken {
 
   enum TokenType {
-    /* Identifier */
+    /* Identifiers */
     
-    // This token type corresponds to any token that is a valid variable name,
-    // beginning with a letter or underscore (a-z, A-Z, _), and continuing with
-    // a sequence of letters, numbers, and underscores until the next character
-    // is not one of those (operator or space, most likely)
+    /// This token type corresponds to any token that is a valid variable name,
+    /// beginning with a letter or underscore (a-z, A-Z, _), and continuing with
+    /// a sequence of letters, numbers, and underscores until the next character
+    /// is not one of those (operator or space, most likely)
     TT_IDENTIFIER,
     
     
     /* Literals */
     
-    // One of two options
-    // ', then a single character other than \, followed by a closing '
-    // '\, then a single valid escape character (\nt0), followed by a closing '
+    /// One of two options
+    /// ', then a single character other than \, followed by a closing '
+    /// '\, then a single valid escape character (\nt0), followed by a closing '
     TT_CHAR_LITERAL,
     TT_INT_LITERAL,
     TT_BOOL_LITERAL,
     TT_FLOAT_LITERAL,
     TT_STRING_LITERAL,
-  };
   
+    
+    /* Operators */
+    TT_PLUS,
+    TT_MINUS,
+    TT_STAR,
+    TT_SLASH,
+    TT_ASSIGN,
+    
+  };
+    
   struct Token {
     TokenType type;
     union {
